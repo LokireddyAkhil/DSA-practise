31f73ce008db614283bb93853dc40a790cf500d3
LokireddyAkhil <akhil.lokireddy@gmail.com>
2025-01-21 21:23:14 +0530

    push0ToEnd.java

---
+++ push0ToEnd.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class push0ToEnd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter size");
+        int size = sc.nextInt();
+        int arr[]= new int[size];
+        System.out.println("enter elements");
+        for (int i = 0; i < arr.length ; i++) {
+            arr[i]=sc.nextInt();
+        }
+        push0s(arr);
+     System.out.println(Arrays.toString(arr));
+
+    }
+    public static void push0s(int arr[]){
+        for(int j =0;j< arr.length-1;j++) {
+            for (int i = 0; i < arr.length - 1 - j; i++) {
+                if (arr[i] == 0) {
+                    int temp = arr[i + 1];
+                    arr[i + 1] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+    // 2nd method.
+    public static void push0ss(int []arr){
+        ArrayList<Integer> k = new ArrayList<>();
+        int nz=0;
+        for (int i = 0; i < arr.length ; i++) {
+            if(arr[i]!=0){
+                k.add(arr[i]);
+            }
+            else{
+                nz++;
+            }
+        }
+        for (int i = 0; i <nz ; i++) {
+            k.add(0);
+        }
+        System.out.println(k);
+
+    }
+}
