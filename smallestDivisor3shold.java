21190ad721175d9bea7403ac3dca1e1209ce1eea
LokireddyAkhil <akhil.lokireddy@gmail.com>
2025-02-01 22:20:59 +0530

    leetcode 1283

--- smallestDivisor3shold.java
+++ smallestDivisor3shold.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
+
 public class smallestDivisor3shold {
     public static void main(String[] args) {
-
+        int[] arr = {44,22,33,11,1};
+        int a = smallestDivisor(arr,5);
+        int b = smallestDivisor1(arr,5);
+        System.out.println(a);
+        System.out.println(b);
     }
     public  static int smallestDivisor(int[] arr, int td) { // may not work for some test case
         // time complexity = O(n+O(d*n))
@@ -24,5 +30,35 @@ public class smallestDivisor3shold {
             }
         }
         return  d;
+    }
+    public static int smallestDivisor1(int[] arr, int td) {
+        // time complexity = O(n*log mx) space complexity = o(1)
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            max = Math.max(max,arr[i]);
+        }
+        int low =1;
+        int high = max;
+        int d=1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            int sum =0;
+            for(int i=0;i<arr.length;i++){
+                if(arr[i]%mid==0){
+                    sum += arr[i]/mid;
+                }
+                else{
+                    sum += arr[i]/mid + 1;
+                }
+            }
+            if(sum<=td){
+                d = mid;
+                high = mid-1;;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return d;
     }
 }
