d28e37a0f6e5a81e209563a30f0228695cde6bbe
LokireddyAkhil <akhil.lokireddy@gmail.com>
2025-02-06 22:37:54 +0530

    modified:   allsubStrings.java

--- allsubStrings.java
+++ allsubStrings.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 
 public class allsubStrings {
     public static void main(String[] args) {
+        System.out.println("enter string");
         Scanner sc = new Scanner(System.in);
         String h = sc.next();
         int k =0;
         for (int i = 0; i <h.length() ; i++) {
-            for (int j = i+1; j <h.length() ; j++) {
-                System.out.print(h.substring(i,j)+" "+ k);
+            for (int j = i+1; j <=h.length() ; j++) {
+                System.out.println(h.substring(i,j));
 
             }
-            System.out.println("");
+
         }
     }
 }
