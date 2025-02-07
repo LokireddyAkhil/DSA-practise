15ae76475afb1da78b74f7dbc142abaa881a8657
LokireddyAkhil <akhil.lokireddy@gmail.com>
2025-02-07 13:50:09 +0530

    leetcode 38

---
+++ CountAndSay.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.SimpleTimeZone;
+
+public class CountAndSay {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int a = sc.nextInt();
+        String s = CAS(a);
+        System.out.println(s);
+
+    }
+    public static String CAS(int n){
+        if(n==1) return "1";
+        String s = CAS(n-1);
+        String ans ="";
+        int i=0;int j=0;
+        while(j<s.length()){
+            if(s.charAt(i)==s.charAt(j)) {
+                j++;
+            }
+            else{
+                int len = j-i;
+                ans += len;
+                ans += s.charAt(i);
+                i=j;
+            }
+
+        }
+        int len = j-i;
+        ans += len;
+        ans += s.charAt(i);
+        return ans;
+    }
+    
+}
