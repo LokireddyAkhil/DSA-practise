3a55afdcb07ae781e9509f80c8e02b75957e7e43
LokireddyAkhil <akhil.lokireddy@gmail.com>
2025-03-07 22:31:28 +0530

    leetcode 155 updated space complexity

--- MinStack.java
+++ MinStack.java
@@ -43,4 +43,60 @@ public class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+
+
+    class MinStack1 {
+        // // t.c = o(1)
+        //    //s.c = o(1)
+        Stack<Long> st = new Stack<>();
+        long min;
+
+        public MinStack1() {
+            min = Long.MAX_VALUE;
+        }
+
+        public void push1(int val) {
+            long x = val;
+            if (st.isEmpty()) {
+                st.push(x);
+                min = x;
+            } else if (x >= min) {
+                st.push(x);
+            } else {
+                st.push(2 * x - min);
+                min = x;
+            }
+        }
+
+        public void pop1() {
+            if (st.isEmpty()) return;
+            if (st.peek() >= min) {
+                st.pop();
+            } else {
+                min = 2 * min - st.pop();
+            }
+        }
+
+        public int top1() {
+            long q = st.peek();
+            if (q >= min) {
+                return (int) q;
+            } else {
+                return (int) min;
+            }
+        }
+
+        public int getMin1() {
+            return (int) min;
+        }
+    }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
 }
